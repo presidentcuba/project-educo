7f59171da28f96f6791d33fd540ac14b07dc2889
duc-bsc <66659272+duc-bsc@users.noreply.github.com>
2022-06-11 10:14:49 -0700

    updating

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,9 +1,11 @@
 import "./styles.scss";
-import { Button, Carousel, Typography } from "antd";
+import { Button, Carousel, Layout, Space, Typography } from "antd";
 import { Link } from "react-router-dom";
 import logo from "src/assets/login-one.svg";
 import logo1 from "src/assets/login-two.svg";
 import logo2 from "src/assets/login-three.svg";
+import { Content } from "antd/lib/layout/layout";
+import Paragraph from "antd/lib/skeleton/Paragraph";
 
 const LoginPage = () => {
   const dataLogin = [
@@ -24,28 +26,34 @@ const LoginPage = () => {
     },
   ];
   return (
-    <div className="login-container">
-      <Typography.Text className="btn-skip">skip</Typography.Text>
-      <Carousel dots={true} autoplay={true} effect="fade">
-        {dataLogin.map((data, index) => (
-          <div className="login-slider" key={index}>
-            <div className="carousel-slide">
-              <img width={210} src={data.image} alt={data.label} />
-              <Typography.Text className="slogan">{data.label}</Typography.Text>
-              <Typography.Text className="description">{data.description}</Typography.Text>
-            </div>
-          </div>
-        ))}
-      </Carousel>
-
-      <Button className="btn btn-register" type="primary">
-        <Link to="/register">Register</Link>
-      </Button>
-
-      <Button className="btn btn-login">
-        <Link to="/login-detail">Log in </Link>
-      </Button>
-    </div>
+    <>
+      <Layout className="login-container">
+        <Content>
+          <Typography.Text className="btn-skip">skip</Typography.Text>
+          <Carousel dots={true} autoplay={true} effect="fade">
+            {dataLogin.map((data, index) => (
+              <div className="login-slider" key={index}>
+                <div className="carousel-slide">
+                  <img width={210} src={data.image} alt={data.label} />
+                  <Typography.Text className="slogan">{data.label}</Typography.Text>
+                  <Typography.Text className="description">{data.description}</Typography.Text>
+                </div>
+              </div>
+            ))}
+          </Carousel>
+          {/* <Link to="/register"> */}
+          <Button className="btn btn-primary btn-register" type="primary" block>
+            Register
+          </Button>
+          {/* </Link> */}
+          {/* <Link to="/login-detail"> */}
+          <Button className="btn btn-secondary btn-login" block>
+            Log in
+          </Button>
+          {/* </Link> */}
+        </Content>
+      </Layout>
+    </>
   );
 };
 export default LoginPage;
