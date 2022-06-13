14cf42ac7ad9dbf8ff3ef17f8f72da4cff110e85
tinhdv <presidentcubacuba@gmail.com>
2022-06-13 23:25:03 +0700

    ui page search/result, component footer

--- src/components/CardSearch/index.js
+++ src/components/CardSearch/index.js
@@ -1,20 +1,27 @@
 import { Typography } from "antd";
 import "./styles.scss";
+import user from "src/assets/user.png";
+import star from "src/assets/star.svg";
 const CardSearch = ({ data }) => {
-  const { image, title, name, position } = data;
+  const { image, title, name, total, rate } = data;
   return (
-    <div className="card-info">
+    <div className="card-search">
       <div className="image">
         <img src={image} alt={title} />
       </div>
-      <div className="name">
-        <Typography.Text>{name ? name : ""}</Typography.Text>
-      </div>
-      <div className="position">
-        <Typography.Text>{position ? position : ""}</Typography.Text>
-      </div>
-      <div className="title">
-        <Typography.Text>{title ? title : ""}</Typography.Text>
+      <div className="info">
+        <Typography.Text className="info-title">
+          {title ? title : ""}
+        </Typography.Text>
+        <Typography.Text className="info-name">
+          {name ? name : ""}
+        </Typography.Text>
+        <div className="rating">
+          <img className="user" src={user} alt="" />
+          <Typography.Text className="total">{total}k student</Typography.Text>
+          <img src={star} alt="star" />
+          <Typography.Text>{rate}</Typography.Text>
+        </div>
       </div>
     </div>
   );
--- src/pages/SearchResult/index.js
+++ src/pages/SearchResult/index.js
@@ -1,44 +1,37 @@
 import "./styles.scss";
-import CardInfo from "src/components/CardInfo";
 import { Typography, Row, Col } from "antd";
 import CustomInput from "src/components/Input";
-import demo from "src/assets/demo.svg";
+import CardSearch from "src/components/CardSearch";
+import demo from "src/assets/searchfilter.png";
 
 const SearchResult = () => {
-  const dataCardInfo = { image: demo, title: "Interior design" };
+  const dataSearch = {
+    image: demo,
+    title: "Adobe illustrator for all beginner artist",
+    name: "Samule Doe",
+    total: 4,
+    rate: 4.7,
+  };
 
-  const dataSearch = ["photography", "craft", "procreate", "UX design"];
   const handleChange = (e) => {
     console.log(e);
   };
   return (
-    <div className="page-search">
+    <div className="page-search-filter">
       <div className="input-search">
         <CustomInput
           onChange={handleChange}
           placeholder="Graphic illustration"
         />
       </div>
-      <div className="top-search">
-        <Typography.Text className="title">
-          Top searchesssssssssssssssssssssssssssssss
-        </Typography.Text>
-        <div className="item">
-          {dataSearch.map((item, index) => (
-            <div className="item-box" key={index}>
-              <Typography.Text>{item}</Typography.Text>
-            </div>
-          ))}
-        </div>
-      </div>
 
-      <div className="categories">
-        <Typography.Text className="title">Categories</Typography.Text>
+      <div className="result">
+        <Typography.Text className="title">Your search result</Typography.Text>
         <div className="item">
           <Row>
-            {[...new Array(8)].map((_, index) => (
-              <Col span={12} key={index}>
-                <CardInfo data={dataCardInfo} />
+            {[...new Array(5)].map((_, index) => (
+              <Col span={24} key={index}>
+                <CardSearch data={dataSearch} />
               </Col>
             ))}
           </Row>
